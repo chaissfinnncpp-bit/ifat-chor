fc5380d0cdcbb16d323bd72f3cb42ad05d51b775
chaissfinnncpp-bit <chaissfinnncpp@gmail.com>
2025-10-25 08:39:23 +0530

    Delete math.py

--- math.py
+++
@@ -1,8 +0,0 @@
-try:
-  a = float(input("Enter first number: "))
-  b = float(input("Enter second number: "))
-
-  print(f"Addition:{a+b}")
-  print(f"Subtraction: {a-b}")
-except ValueError:
-  print("Please enter vaild number")
